e6ceedbe5ea6033e3809de8dc540b93b9f21ce30
Your Name <iyanperezmarcos@gmail.com>
2023-02-06 09:28:49 +0100

    feeding method done

--- src/main/java/com/kata/App.java
+++ src/main/java/com/kata/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        Tamagotchi Pou = new Tamagotchi();
+        Pou.feeding(Pou);
+        
+        System.out.println(Pou.getHunger());
     }
 }
--- src/main/java/com/kata/Tamagotchi.java
+++ src/main/java/com/kata/Tamagotchi.java
@@ -4,6 +4,62 @@ public class Tamagotchi {
     
     int mood = 4;
     int hunger = 4;
+    int fullness = 4;
     int energy = 4;
 
+    
+
+    public Tamagotchi() {
+
+    }
+
+
+
+    public void feeding(Tamagotchi tamagotchi){
+        if(tamagotchi.getFullness()<100){
+            tamagotchi.setHunger(tamagotchi.getHunger()+2);
+            tamagotchi.setFullness(tamagotchi.getFullness()-2);
+        }
+        if(tamagotchi.getFullness()>=100){
+            System.out.println("El pou esta lleno");
+        }
+    }
+
+
+
+
+
+    public int getMood() {
+        return mood;
+    }
+
+    public void setMood(int mood) {
+        this.mood = mood;
+    }
+
+    public int getHunger() {
+        return hunger;
+    }
+
+    public void setHunger(int hunger) {
+        this.hunger = hunger;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+
+    public int getFullness() {
+        return fullness;
+    }
+
+    public void setFullness(int fullness) {
+        this.fullness = fullness;
+    }
+
 }
+
